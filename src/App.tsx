c1c401f576d874aa9580d9d4d37f5d7ea55f2af1
Darin Cassler <darin@cassler.net>
2022-02-17 11:16:00 -0600

    add clientID secrets

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@auth0/auth0-react'
 import { useState } from 'react'
 import { AppHeader } from './Layout'
 import logo from './logo.svg'
@@ -8,6 +9,11 @@ function App() {
   const [count, setCount] = useState(0)
   const [bonus, toggleBonus] = useState(false);
   return (
+     <Auth0Provider
+      domain="cassler.auth0.com"
+      clientId={import.meta.env.VITE_AUTH0_CLIENTID as string}
+      redirectUri={window.location.origin}
+    >
     <div className="min-h-screen dark:from-brand-300 dark:to-brand-900 from-brand-400 to-brand-100 bg-gradient-to-bl transition-all duration-250">
       <AppHeader />
       <main className="border-8 border-blue-700 justify-center items-center text-center min-h-screen flex flex-col">
@@ -19,6 +25,7 @@ function App() {
           )}
       </main>
     </div>
+    </Auth0Provider>
   )
 }
 
