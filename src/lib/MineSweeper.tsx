8dd1499856a56459c904661c9ba3e5387d057dae
Darin Cassler <darin@cassler.net>
2022-02-18 10:11:05 -0600

    cleanup

--- src/lib/MineSweeper.tsx
+++ src/lib/MineSweeper.tsx
@@ -23,51 +23,51 @@ export function MineSweeper() {
 
   const GameOverScreen = () => (
     <div className="ring-4 ring-red-500 animate-fadeIn transition-all duration-200 bg-white/90 text-center hover:scale-110 rotate-2 rounded-lg drop-shadow-lg text-black w-[320px] p-8 space-y-2">
-          <Dialog.Overlay />
-          <Dialog.Title className="text-black space-y-2 justify-center">
-            <div className="text-xl font-semibold pb-2">Your score</div>
-            <div className="text-black text-[64px]">{flippedItems.length}</div>
-          </Dialog.Title>
-          <p className="text-sm leading-2 pt-8 px-2 text-center">
-            You only had
-            {' '}
-            {board.length - flippedItems.length}
-            {' '}
-            left to go! There were
-            {' '}
-            {board.filter((i) => i.bomb).length}
-            {' '}
-            bombs, and you found one.
-            {' '}
-            <span className="font-bold">You Lose.</span>
-          </p>
-          <div title="actions-menu" className="pt-8 flex items-center">
-            <button
-              type="button"
-              className="
-              py-2 transition-all hover:scale-110 hover:-translate-y-2
-              duration-300 ease-in-out hover:rotate-2
-              px-4 ring ring-purple-700"
-              onClick={() => handleClose()}
-            >
-              Good Game
+      <Dialog.Overlay />
+      <Dialog.Title className="text-black space-y-2 justify-center">
+        <div className="text-xl font-semibold pb-2">Your score</div>
+        <div className="text-black text-[64px]">{flippedItems.length}</div>
+      </Dialog.Title>
+      <p className="text-sm leading-2 pt-8 px-2 text-center">
+        You only had
+        {' '}
+        {board.length - flippedItems.length}
+        {' '}
+        left to go! There were
+        {' '}
+        {board.filter((i) => i.bomb).length}
+        {' '}
+        bombs, and you found one.
+        {' '}
+        <span className="font-bold">You Lose.</span>
+      </p>
+      <div title="actions-menu" className="pt-8 flex items-center">
+        <button
+          type="button"
+          className="
+          py-2 transition-all hover:scale-110 hover:-translate-y-2
+          duration-300 ease-in-out hover:rotate-2
+          px-4 ring ring-purple-700"
+          onClick={() => handleClose()}
+        >
+          Good Game
 
-            </button>
-            <button
-              type="button"
-              className="py-2 transition-all hover:scale-110
-              hover:-translate-y-2 duration-300
-              ease-in-out hover:-rotate-2
-              bg-purple-500  ring-purple-700 text-white px-4 ring ml-4"
-              onClick={() => handleClose()}
-            >
-              Play Again
-            </button>
-          </div>
-          <Dialog.Description className="text-center text-xs text-black/50 pt-8">
-            Thanks for playing.
-          </Dialog.Description>
-        </div>
+        </button>
+        <button
+          type="button"
+          className="py-2 transition-all hover:scale-110
+          hover:-translate-y-2 duration-300
+          ease-in-out hover:-rotate-2
+          bg-purple-500  ring-purple-700 text-white px-4 ring ml-4"
+          onClick={() => handleClose()}
+        >
+          Play Again
+        </button>
+      </div>
+      <Dialog.Description className="text-center text-xs text-black/50 pt-8">
+        Thanks for playing.
+      </Dialog.Description>
+    </div>
   )
   return (
     <BoardContext.Provider value={ctx}>
