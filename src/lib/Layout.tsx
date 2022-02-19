4d53c15d1227363e11f5a8b5c8a2af5f3aaaffad
Darin Cassler <darin@cassler.net>
2022-02-18 20:36:45 -0600

    cleanup ga a bit

--- src/lib/Layout.tsx
+++ src/lib/Layout.tsx
@@ -11,28 +11,32 @@ import { useAuth0 } from '@auth0/auth0-react';
 export function AppHeader({className = ''}: {className:string}) {
   const { darkMode, toggle } = useDarkMode();
   const { user, isLoading } = useAuth0();
+  const {bonus} = useContext(BonusContext);
+
   useEffect(() => {
-    ReactGA.initialize(import.meta.env.VITE_ANALYTICS_ID as string, {
-      debug: true,
-    });
+    ReactGA.initialize(import.meta.env.VITE_ANALYTICS_ID as string, {debug: true});
     ReactGA.pageview('/')
   }, [])
+
+  useEffect(() => {
+    ReactGA.pageview(bonus ? '/minesweeper' : '/')
+    ReactGA.event({
+      category: 'Easter Egg',
+      action: 'Toggle Mode',
+    })
+  }, [bonus])
 
   useEffect(() => {
     if ( isLoading ) return
     ReactGA.set({ userId: user?.sub })
   }, [user])
 
-  const Logo = () => useMemo(() => (
-    <div><img src={darkMode ? lightLogo : logoUrl} className='h-8 w-auto translate-y-1 opacity-90'/></div>
-  ), [darkMode, toggle])
-
   return (
     <header className={className}>
       <div className="container flex">
         <a href="https://striveconsulting.com/" target="_blank" className="flex-1 pb-1.5">
           <div className='flex items-center gap-2 hover:-translate-y-1 transition-all duration-250'>
-          <Logo />
+          <div><img src={darkMode ? lightLogo : logoUrl} className='h-8 w-auto translate-y-1 opacity-90'/></div>
           <div className='flex flex-col items-left text-left justify-center -space-y-2 text-black/75 dark:text-white/90 dark:hover:text-white'>
             <div className='text-2xl font-normal'>strive</div>
             <div className='font-semibold text-xs tracking-tighter flex-nowrap whitespace-nowrap'>Innovation Lab</div>
@@ -53,13 +57,6 @@ export function AppHeader({className = ''}: {className:string}) {
 
 export function Layout({children}:React.PropsWithChildren<{}>) {
   const {bonus} = useContext(BonusContext);
-
-  useEffect(() => {
-    ReactGA.event({
-      category: 'Easter Egg',
-      action: 'Toggle Mode',
-    })
-  }, [bonus])
   return (
     <div className={`App-backdrop`}>
       <main className={`App-container ${bonus ? 'electric-dream' : ''}`}>
--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
         extend(),
         nesting(),
         tailwindcss(),
-        // autoprefixer(),
         apply(),
         postcssimport()
       ],
